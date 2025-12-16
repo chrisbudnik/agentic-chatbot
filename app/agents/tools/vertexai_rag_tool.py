c752caed412bd87cda67ef4d3dda27c8c002437c
chrisb <chrism.budnik@gmail.com>
2025-12-16 22:32:34 +0100

    create: vertex ai rag search tool

---
+++ app/agents/tools/vertexai_rag_tool.py
@@ -0,0 +1,73 @@
+import httpx
+from pydantic import BaseModel
+from google.auth import default
+from google.auth.transport.requests import Request
+
+from app.core.config import settings
+from app.agents.tools.base import BaseTool
+
+
+class DiscoveryEngineClient:
+    """
+    Use Vertex AI Discovery Engine and AI Search Application for document retrieval.
+    """
+    def __init__(self, app_engine: str) -> None:
+        creds, project_id = default()
+        auth_req = Request()
+        creds.refresh(auth_req)
+
+        self.project_id = project_id
+        self.app_engine = app_engine
+        self.access_token = creds.token
+
+    def search(self, query: str):
+        """
+        Performs a search query against the Discovery Engine API.
+        """
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json"
+        }
+
+        url = (
+            f"https://discoveryengine.googleapis.com/v1alpha/projects/"
+            f"{self.project_id}/locations/global/collections/default_collection/"
+            f"engines/{self.app_engine}/servingConfigs/default_search:search"
+        )
+
+        data = {
+            "query": f"{query}",
+            "pageSize": 10,
+            "queryExpansionSpec": {"condition": "AUTO"},
+            "spellCorrectionSpec": {"mode": "AUTO"},
+            "contentSearchSpec": {
+                "summarySpec": {
+                    "ignoreAdversarialQuery": True,
+                    "includeCitations": False,
+                    "summaryResultCount": 10,
+                    "languageCode": "pl"
+                }
+            }
+        }
+        response = httpx.post(url, headers=headers, json=data)
+        return response.json()
+
+
+
+class VertexAIRagTool(BaseTool):
+    name = "search_tool"
+    description = "Searches the database for information."
+
+    class Input(BaseModel):
+        query: str
+
+    input_schema = Input
+
+    async def run(self, query: str):
+        """
+        Runs a search query using Vertex AI Discovery Engine.
+        """
+        client = DiscoveryEngineClient(app_engine=settings.VERTEXAI_APP_ENGINE_ID)
+        results = client.search(query)
+        return results
